3e25adaa679fb58c68d61521b6a7f93a724aac10
jihye <jiji.pak41@gmail.com>
2021-08-23 22:59:01 +0100

    url test second attempt using equal function

--- week9/learn-unit-testing/workshop/index.test.js
+++ week9/learn-unit-testing/workshop/index.test.js
@@ -1,7 +1,29 @@
 test('The URL is correct', () => {
-  if (makeUrl('pikachu') === 'https://pokeapi.co/api/v2/pikachu') {
-    return console.log('correct');
-  } else {
-    return console.error('wrong');
-  }
+  equal(makeUrl('pikachu'), 'https://pokeapi.co/api/v2/pikachu');
 });
+
+// function equal(actual, expected, message) {
+//   if (actual === expected) {
+//     const defaultMessage = `Expected ${expected} and received ${actual}`;
+//     console.info("Pass: " + (message || defaultMessage));
+//   } else {
+//     const defaultMessage = `Expected ${expected} but received ${actual} instead`;
+//     console.error("Fail: " + (message || defaultMessage));
+//   }
+// }
+
+// function notEqual(actual, expected, message) {
+//   if (actual !== expected) {
+//     const defaultMessage = `${expected} is different to ${actual}`;
+//     console.info("Pass: " + (message || defaultMessage));
+//   } else {
+//     const defaultMessage = `${expected} is the same as ${actual}`;
+//     console.error("Fail: " + (message || defaultMessage));
+//   }
+// }
+
+// function test(name, testFunction) {
+//   console.group(name);
+//   testFunction();
+//   console.groupEnd(name);
+// }
